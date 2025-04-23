02608b3be589a22d50f69a7ddd77a4c1643be972
michaliskaratsiorispartner <michalis.karatsioris@partner.bmwgroup.com>
2025-04-23 14:42:48 +0200

    implemented the lomuto partition scheme in quick_sort.py

--- algotithms/quick_sort.py
+++ algotithms/quick_sort.py
@@ -1,8 +1,9 @@
-from typing import Any
+from typing import Any, Literal
 from collections.abc import Iterable
 
 def swap(a: int, b: int, arr: Iterable) -> None:
-    """_summary_
+    """
+    Swapping two elements of an iterable.
 
     Parameters
     ----------
@@ -18,12 +19,13 @@ def swap(a: int, b: int, arr: Iterable) -> None:
         arr[a] = arr[b]
         arr[b] = tmp
 
-def partition(test: Iterable, start: int, end: int) -> int:
-    """_summary_
+def hoare_partition(iterable: Iterable, start: int, end: int) -> int:
+    """
+    Hoare partition scheme.
 
     Parameters
     ----------
-    test : Iterable
+    iterable : Iterable
         _description_
     start : int
         _description_
@@ -36,37 +38,69 @@ def partition(test: Iterable, start: int, end: int) -> int:
         _description_
     """
     pivot_index: int = start
-    pivot: Any = test[pivot_index]
+    pivot: Any = iterable[pivot_index]
     while start < end:
-        while start < len(test) and test[start] <= pivot:
+        while start < len(iterable) and iterable[start] <= pivot:
             start += 1
-        while test[end] > pivot:
+        while iterable[end] > pivot:
             end -= 1
         if start < end:
-            swap(start, end, test)
-    swap(pivot_index, end, test)
+            swap(start, end, iterable)
+    swap(pivot_index, end, iterable)
     return end
 
-def quick_sort(test: Iterable, start: int, end: int) -> None:
+def lomuto_partition(iterable: Iterable, start: int, end: int) -> int:
     """
-    Quick Sort Algorithm implementing Hoare partition scheme.
+    Lomuto partition scheme.
+
+    Parameters
+    ----------
+    iterable : Iterable
+        _description_
+    start : int
+        _description_
+    end : int
+        _description_
+
+    Returns
+    -------
+    int
+        _description_
+    """
+    pivot = iterable[end]
+    p_index = start
+
+    for i in range(start, end):
+        if iterable[i] <= pivot:
+            swap(i, p_index, iterable)
+            p_index += 1
+    swap(p_index, end, iterable)
+    return p_index
+
+def quick_sort(iterable: Iterable, start: int, end: int, partition_scheme: Literal['hoare', 'lomuto'] = 'hoare') -> None:
+    """
+    Quick Sort Algorithm
     
     Time complexity:    O(logN)
     Space complexity:   O(1)
 
     Parameters
     ----------
-    test : Iterable
+    iterable : Iterable
         _description_
     start : Any
         _description_
     end : Any
         _description_
+    partition_scheme : str
+        Partition scheme to implement. Acceptable values are 'hoare' and 'lomuto' with the default value set to 'hoare'.
     """
+    if len(iterable)== 1:
+        return
     if start < end:
-        pi: int = partition(test, start, end)
-        quick_sort(test, start, pi-1)
-        quick_sort(test, pi+1, end)
+        pi: int = lomuto_partition(iterable, start, end) if partition_scheme == 'lomuto' else hoare_partition(iterable, start, end)
+        quick_sort(iterable, start, pi-1)
+        quick_sort(iterable, pi+1, end)
 
 if __name__ == '__main__':
     tests = [
@@ -80,8 +114,11 @@ if __name__ == '__main__':
         ["Michalis", "michalis", "miChalis", "MiChalis"]
     ]
 
-    for test in tests:
+    for index, test in enumerate(tests):
         print(f"{test=}")
-        quick_sort(test, 0, len(test)-1)
-        print(f'sorted array: {test}\n')
+        partition_scheme: str = 'lomuto' if index % 2 == 0 else 'hoare'
+        quick_sort(test, 0, len(test)-1, partition_scheme='lomuto')
+        quick_sort(test, 0, len(test)-1, partition_scheme='hoare')
+        print(f'sorted array: {test}')
+        print(f"{partition_scheme=}\n")
 
